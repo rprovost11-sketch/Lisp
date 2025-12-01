e8e31b9cf5625e062ebe208afb58ecbaa5e46c42
Ronald Longo <ronlongo9@outlook.com>
2025-12-01 17:28:27 -0500

    Final modification to the verion number.

--- Lisp.py
+++ Lisp.py
@@ -4,8 +4,9 @@ from LispInterpreter import LispInterpreter
 def main( ) -> None:
    interp = LispInterpreter( )
    theListener = Listener( interp, language='Python\'s Lisp',
-                                   version='0.1.5',
+                                   version='0.1.6',
                                    author='Ronald Provost/Longo',
+                                   email='ronLongo9@outlook.com',
                                    libdir='lib',
                                    testdir='testing'
                                    )
