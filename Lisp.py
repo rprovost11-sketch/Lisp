c0dd97527bedd55a5d86d612cf611937dd7ed3df
Ronald Longo <ronlongo9@outlook.com>
2025-12-12 18:37:19 -0500

    forgot to change the software version number.

--- Lisp.py
+++ Lisp.py
@@ -4,7 +4,7 @@ from LispInterpreter import LispInterpreter
 import sys
 
 LANGUAGE     = 'Python\'s Lisp'
-VERSION      = '0.5.0'
+VERSION      = '0.6.0'
 AUTHOR       = 'Ronald Provost/Longo'
 EMAIL        = 'ronLongo9@outlook.com'
 TEST_DIR     = 'testing'
