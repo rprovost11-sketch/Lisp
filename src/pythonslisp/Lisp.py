7dcd2fcfdb8a19cefa0da11543c348b10fe42589
Ronald Longo <ronlongo9@outlook.com>
2025-12-13 08:58:11 -0500

    MOdifications to fix the package importing and execution operations.

--- src/pythonslisp/Lisp.py
+++ src/pythonslisp/Lisp.py
@@ -4,7 +4,7 @@ from LispInterpreter import LispInterpreter
 import sys
 
 LANGUAGE     = 'Python\'s Lisp'
-VERSION      = '0.6.0'
+VERSION      = '0.7.1'
 AUTHOR       = 'Ronald Provost/Longo'
 EMAIL        = 'ronLongo9@outlook.com'
 TEST_DIR     = 'testing'
---
+++ src/pythonslisp/__main__.py
@@ -0,0 +1,5 @@
+
+from Lisp import main
+
+if __name__ == '__main__':
+   main( )
