a41ef752ad98999890cbf818568581d1b12860ca
Ronald Longo/Provost <rprovost11@gmail.com>
2026-02-22 18:22:55 -0500

    Upped the version number.

--- pythonslisp/__main__.py
+++ pythonslisp/__main__.py
@@ -5,7 +5,7 @@ from pythonslisp.Listener import Listener
 from pythonslisp.LispInterpreter import LispInterpreter
 
 LANGUAGE     = 'Python\'s Lisp'
-VERSION      = '0.27.3'
+VERSION      = '0.27.4'
 AUTHOR       = 'Ronald Provost/Longo'
 EMAIL        = 'ronLongo9@outlook.com'
 PROJECT = 'https://github.com/rprovost11-sketch/Lisp'
